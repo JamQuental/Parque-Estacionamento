41a2ae20d93a5292ff87ccea2bd0864316fc6dcc
JamQuental <jamquental@gmail.com>
2022-10-10 15:35:21 +0100

    Gere inicio

--- ParqueEstacionamento/src/project/GereParque.java
+++ ParqueEstacionamento/src/project/GereParque.java
@@ -1,5 +1,23 @@
 package project;
 
+import java.util.ArrayList;
+
 public class GereParque {
+	private ArrayList<Aluno> alunos;
+	public GereParque() {
+		alunos = new ArrayList<Aluno>();
+	}
+	
+	public void criarAluno(String nome, String carro) {
+		
+	}
+	
+	public void adicionarPagamento(int tempo, String nome) {
+		
+	}
+	
+	public void calcularDescontro() {
+		
+	}
 
 }
